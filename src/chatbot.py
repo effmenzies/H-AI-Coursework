68b1da9f0ed23a055118dcfccab15b96ed69c498
effmenzies <effmenzies@gmail.com>
2024-11-12 15:46:12 +0000

    asking for names

--- src/chatbot.py
+++ src/chatbot.py
@@ -9,6 +9,7 @@ from nltk import *
 from database import *
 from qanda import *
 from smalltalk import *
+from user_input import *
 
 class Chatbot:
     def __init__(self, database):
@@ -16,6 +17,7 @@ class Chatbot:
         self.sentiment = 0
         nltk.download('wordnet')
         nltk.download('averaged_perceptron_tagger_eng')
+        nltk.download('universal_target')
         build_dt_matrix(questions)
         smalltalk_init('smalltalk')
     
@@ -24,44 +26,57 @@ class Chatbot:
         database.connect()
         prompts = ["How are you today?","What's the weather like today?","What's the time?","What can you do for me?",
                    "What's your favourite food?","Recommend things to do near me."]
-        userInput = None
         try:
             #check if user  1 exists
             database.sqlexecute('''SELECT * FROM UserInfo WHERE userID=1''')
             user = database.cursor.fetchone()
-
+            userName=None
+            print(f"<{self.name}>: Hi! My name is {self.name}.")
             if user:
                 #greet user
+                userName = user[1]
                 print(f"\nTo exit the chat, type 'exit'\n")
-                print(f"<{self.name}>: Hi {user[1]}!")
+                print(f"<{self.name}>: Hi {userName}!")
             else:
-                #ask for name
-                print(f"<{self.name}>: Hi! My name is {self.name}.\n What's your name?")
-                userInput = input("<you>: ")
-                #insert username into database
-                database.sqlexecute(f'''INSERT INTO UserInfo (name) VALUES ('{userInput}')''')
-                print(f"<{self.name}>: Hi {userInput}!\n To exit the chat, type 'exit'.")
-                database.sqlexecute('''SELECT * FROM UserInfo WHERE userID=1''')
-                user = database.cursor.fetchone()
-
+                while not userName:
+                    #ask for name
+                    print(f"What's your name?")
+                    userInput = input("<you>: ")
+                    names = extract_names(userInput)
+                    fullName = max(names)
+                    print(f"<{self.name}>: Hi, I've got your full name as {fullName}.")
+                    if len(names)>1:
+                        nickName = min(names)
+                        print(f"And {nickName} as a nickname.")
+                    print(f"Is this correct?")
+                    userInput = input("<you>: ")
+                    if response(userInput, 'smalltalk')==('confirmation','yes'):
+                        #insert username into database
+                        database.sqlexecute(f'''INSERT INTO UserInfo (name) VALUES ('{fullName}')''')
+                        database.sqlexecute('''SELECT * FROM UserInfo WHERE userID=1''')
+                        user = database.cursor.fetchone()
+                        userName = nickName
+                        print(f"<{self.name}>: Hi {userName}!\n To exit the chat, type 'exit'.")
+                    else:
+                        print(f"<{self.name}>: Sorry, I misunderstood.")
             while True:
                 #main loop
                 print(f"<{self.name}>: How can I help you today?")
                 print(f"\033[3m\033[34mNot sure what to ask?\nTry:\n{random.choice(prompts)}\033[0m")
-                userInput = input(f"<{user[1]}>: ").lower()
+                while True:
+                    #conversation loop
+                    userInput = input(f"<{userName}>: ").lower()
 
-                if userInput == 'exit':
-                    print(f"<{self.name}>: Goodbye!")
-                    break
+                    if userInput == 'exit':
+                        print(f"<{self.name}>: Goodbye!")
+                        break
 
-                output = find_response(userInput)
-                while (not output) & (userInput!='exit'):
-                    #smalltalk loop
-                    print(f"<{self.name}>: I didn't understand that, can you say it again?")
-                    userInput = input(f"<{user[1]}>: ")
                     output = find_response(userInput)
-                if output:
+                    while not output:
+                        print(f"<{self.name}>: I didn't understand that, can you say it again?")
+                        userInput = input(f"<{user[1]}>: ")
+                        if output:
+                            break
                     print(f"<{self.name}>: {output}")
-
         finally:
             database.connection.close()
--- src/user_input.py
+++ src/user_input.py
@@ -1,13 +1,22 @@
-import nltk, string
+import nltk, string, pandas as pd
 from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
+import re
 
-def process_input(input):
+def remove_fluff(input):
+    tokens = word_tokenize(input)
+    greetings = pd.read_csv('data/greetings.csv')
+    for word in tokens:
+        if word.lower() in greetings['Greeting'].values:
+            tokens.remove(word)
+    return " ".join(tokens)
+
+def lemmatize(input):
     lem = WordNetLemmatizer()
     tag_dict = {'NOUN': wordnet.NOUN, 'VERB': wordnet.VERB,'ADV':wordnet.ADV,'ADJ':wordnet.ADJ}
-    input = nltk.pos_tag([word for word in word_tokenize(input) if word not in string.punctuation], tagset='universal')
+    input = tag_input(input)
     input_tokens=[]
     for token in input:
         word= token[0]
@@ -17,5 +26,16 @@ def process_input(input):
         else: input_tokens.append(lem.lemmatize(word))
     return input_tokens
 
+def tag_input(input):
+    return nltk.pos_tag([word for word in word_tokenize(input) if word not in string.punctuation], tagset='universal')
 
-process_input('hello, how are you doing today?')
+def extract_nouns(input):
+    i = remove_fluff(input)
+    return [word[0] for word in tag_input(i) if (word[1]=='NOUN') & (word[0]!='hello')]
+
+def extract_names(input):
+    pattern = r"(?:call me|my (?:full )?name(?:'s| is)|i'm|i (?:like|prefer)(?: to be called)?)\s+([A-Za-z]+)"
+    names = re.findall(pattern, input, re.IGNORECASE)
+    return names
+
+print(extract_names("Sure, my name's Effie but I prefer Eff"))
